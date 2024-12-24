f615d4cf17600a72939b4fd11cf3b3ed5b2e6b22
Winnerkorea <68183508+Winnerkorea@users.noreply.github.com>
2024-12-24 13:15:34 +0900

    3.3.3 플레이어 컨트롤 추가
    플레이어 컨트롤 추가

--- player.gd
+++ player.gd
@@ -1,7 +1,13 @@
 extends RigidBody2D
 
+@export var engine_power = 500
+@export var spin_power = 8000
+
 enum {INIT, ALIVE, INVULNERABLE, DEAD}
 var state = INIT
+var thrust = Vector2.ZERO
+var rotation_dir = 0
+
 
 func _ready() -> void:
 	change_state(ALIVE)
@@ -17,3 +23,18 @@ func change_state(new_state):
 		DEAD:
 			$CollisionShape2D.set_deferred("disabled", false)
 	state = new_state
+
+func _process(delta: float) -> void:
+	get_input()
+	
+func get_input():
+	thrust = Vector2.ZERO
+	if state in [DEAD, INIT]:
+		return
+	if Input.is_action_pressed("thrust"):
+		thrust = transform.x * engine_power
+	rotation_dir = Input.get_axis("rotate_left","rotate_right")
+	
+func _physics_process(delta: float) -> void:
+	constant_force = thrust
+	constant_torque = rotation_dir * spin_power
