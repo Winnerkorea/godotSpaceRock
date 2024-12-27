38a6beb9014fc2425ed101a7211662a91570a26a
Winnerkorea <68183508+Winnerkorea@users.noreply.github.com>
2024-12-27 19:57:20 +0900

    3.5 게임 종료

--- player.gd
+++ player.gd
@@ -28,12 +28,19 @@ func change_state(new_state):
 	match  new_state:
 		INIT:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
 		ALIVE:
 			$CollisionShape2D.set_deferred("disabled", false)
+			$Sprite2D.modulate.a = 1.0
 		INVULNERABLE:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
+			$InvulnerabilityTimer.start()
 		DEAD:
 			$CollisionShape2D.set_deferred("disabled", false)
+			$Sprite2D.hide()
+			linear_velocity = Vector2.ZERO
+			dead.emit()
 	state = new_state
 
 func _process(delta: float) -> void:
@@ -88,4 +95,8 @@ func reset():
 	reset_pos = true
 	$Sprite2D.show()
 	lives = 3
+	change_state(ALIVE)
+
+
+func _on_invulnerability_timer_timeout() -> void:
 	change_state(ALIVE)
